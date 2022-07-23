6bbfe04f1b91aa567ff062c7e4d46f1e6d99b578
M Afif Nur Azizi <afifnurazizi@gmail.com>
2022-07-23 21:07:23 +0800

    feat: add c01_examp1_variable.py #7
    Writing tutorialn on how to create variable in python

---
+++ c01_basic/c01_examp1_variable.py
@@ -0,0 +1,30 @@
+"------------ VARIABLE ------------"
+
+"1. Cara menulis variable dalam bahasa pemrograman pyhton"
+
+'Pada saat membuat variable, ikutilah format berikut \
+    "nama_variable = nilai"'
+
+">>> Cara menulis variable yang di perbolehkan : "
+
+nama_makanan = "Klepon Manis"
+namaMakanan="Klepon Manis"
+namamakanan="Klepon Manis"
+NAMA_MAKANAN = 'Klepon Manis'
+
+">>> Menampilkan nilai dari sebuah variable dengan menggunakan print() "
+
+print(nama_makanan)
+
+"""
+Hal-hal yang tidak di perkenankan saat menulis sebuah variable:
+# 1. Menggunakan angka diawal variable
+
+1nama_makanan= 'Jenang Kudus'
+
+# 2. Menggunakan operator aritmatika
+nama-makanan = 'Bakwan Jagung'
+
+# 3. Menggunakan spasi
+nama makanan = 'Plecing Kangkung'
+"""
